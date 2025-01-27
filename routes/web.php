ee025ef487ff2e86906b3d8573ab8d311418d63c
azharutama <azharutama837@gmail.com>
2025-01-27 13:14:36 +0700

    make dashboard ui and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('home');
 });
+
+
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
