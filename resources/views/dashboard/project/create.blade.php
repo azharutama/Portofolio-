2f56e04cd23c72775da8c0f0a0adfe994619120f
azharutama <azharutama837@gmail.com>
2025-02-07 19:11:58 +0700

    update migration and crud page

--- resources/views/dashboard/project/create.blade.php
+++ resources/views/dashboard/project/create.blade.php
@@ -5,7 +5,7 @@
     <main class="p-4 md:ml-64 h-auto pt-20">
       <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add Project</h2>
             <form action="#">
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <div class="sm:col-span-2">
