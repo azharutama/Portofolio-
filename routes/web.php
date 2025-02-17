712d38c310cecbf8840c64f7e31b5f0534d9a090
azharutama <azharutama837@gmail.com>
2025-02-17 20:26:24 +0700

    Add login page and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 
 
@@ -17,5 +18,6 @@ Route::get('/dashboard/skill/update', [DashboardController::class, 'Skilupdatels
 Route::get('/dashboard/project/delete', [DashboardController::class, 'deleteProjects']);
 Route::get('/dashboard/skill/delete', [DashboardController::class, 'deleteSkills']);
 Route::get('/dashboard/contact', [DashboardController::class, 'showContact']);
+Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/', [MainController::class, 'index']);
