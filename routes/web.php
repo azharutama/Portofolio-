fc744220c0e345b7959f28eb994a7c080bf63324
azharutama <azharutama837@gmail.com>
2025-03-13 23:14:02 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,27 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 
-
-
+Route::get('/', [MainController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/dashboard/project', [DashboardController::class, 'showProjects']);
 Route::get('/dashboard/skill', [DashboardController::class, 'showSkills']);
+Route::get('/dashboard/contact', [DashboardController::class, 'showContact']);
+
 Route::get('/dashboard/project/create', [DashboardController::class, 'createProjects']);
 Route::get('/dashboard/skill/create', [DashboardController::class, 'createSkills']);
-Route::get('/dashboard/project/update', [DashboardController::class, 'updateProjects']);
-Route::get('/dashboard/skill/update', [DashboardController::class, 'Skilupdatels']);
-Route::get('/dashboard/project/delete', [DashboardController::class, 'deleteProjects']);
-Route::get('/dashboard/skill/delete', [DashboardController::class, 'deleteSkills']);
-Route::get('/dashboard/contact', [DashboardController::class, 'showContact']);
-Route::get('/login', [LoginController::class, 'index']);
-
 Route::post('/dashboard/project', [DashboardController::class, 'storeProjects']);
 Route::post('/dashboard/skill', [DashboardController::class, 'storeSkills']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout']);
 Route::post('/dashboard/contact', [DashboardController::class, 'storeContact']);
-Route::post('/dashboard/project/update', [DashboardController::class, 'storeUpdateProjects']);
-Route::post('/dashboard/skill/update', [DashboardController::class, 'storeUpdateSkills']);
 
+Route::get('/dashboard/project/update/{id}', [DashboardController::class, 'updateProjects']);
+Route::get('/dashboard/skill/update/{id}', [DashboardController::class, 'updateSkills']);
+Route::post('/dashboard/project/update/{id}', [DashboardController::class, 'storeUpdateProjects']);
+Route::post('/dashboard/skill/update/{id}', [DashboardController::class, 'storeUpdateSkills']);
 
-
-Route::get('/', [MainController::class, 'index']);
+Route::delete('/dashboard/project/{id}', [DashboardController::class, 'deleteProjects'])->name('projects.destroy');
+Route::delete('/dashboard/skill/{id}', [DashboardController::class, 'deleteSkills'])->name('skills.destroy');
+Route::delete('/dashboard/contact/{id}', [DashboardController::class, 'deleteContact'])->name('contact.destroy');
