0b0f7969d14e8cb5b4d50aa8025bcdb3aad82740
azharutama <azharutama837@gmail.com>
2025-03-16 23:35:55 +0700

    add function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,11 @@ class MainController extends Controller
     {
         $projects = Project::all(); // Ambil semua data project dari database
         $skills = Skill::all(); // Ambil semua data skill dari database
-        return view('main', compact('skills', 'projects')); // Kirim ke view index
+        $contacts = Contact::all(); // Ambil semua data contact dari database
+
+        return view('main', compact('skills', 'projects', 'contacts')); // Kirim ke view index
+
+
 
     }
 
